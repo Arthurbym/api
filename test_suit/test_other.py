9c1a5639e4bd61ccf262a08e4b85eb0ae1662485
Arthurbym <mtbsw54321@126.com>
2023-04-14 14:43:46 +0800

    '0414443'

---
+++ test_suit/test_other.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from common.do_excel import DoExcel
+from common.do_sql import DoSql
+from common.do_request import DoRequest
+import allure, pytest,time,json
+from common.log import Logger
+from config.config_data import headers
+
+
+log = Logger(__name__).get_logger()
+
+data = DoExcel().get_api_list()
+
+# @allure.epic('自动化接口测试')
+class TestOther():
+    @allure.title('取消匹配')
+    def test_called(self):
+        with allure.step('发起接口请求'):
+            res = json.loads(DoRequest().post_url('/app/mind/match',json={"targetMileage":1600,"userId":343},headers =headers ).text)
+            # uniqueCode = res["data"]['uniqueCode']
+            res1 = DoRequest().post_url('/app/mind/cancel/match', json={"userId":343},headers =headers).text
+        with allure.step('发起接口请求'):
+            assert '"code":"200"' in res1
