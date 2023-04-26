5485be5b962c036e6b6da73cd06d38070bffbaf3
Arthurbym <mtbsw54321@126.com>
2023-04-26 17:16:20 +0800

    '04261716'

--- config/config_data.py
+++ config/config_data.py
@@ -9,7 +9,7 @@ from common.do_config import DoConfigIni
 import  time,base64
 from base64 import b64decode, b64encode
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import pad,unpad
 
 
 doc = DoConfigIni()
@@ -59,62 +59,27 @@ def get_conf():
             return 'get environment failed!!!'
 
 # 获取签名
-def get_sign(headers):
-    strhe = 'zns&'
-    for i in headers.items():
-        if i[0] == 'token':
-            strhe = strhe + str(i[1])+'&'
-        else:
-            strhe = strhe+ str(i[0]) + '=' +str(i[1]) +'&'
-    strhe = strhe.rstrip('&')
-    print(strhe)
-    return strhe
-
-
-# 将原始的明文用空格填充到16字节
-# def pad(data):
-#     block_size = 16
-#     # 数据进行 PKCS5Padding 的填充
-#     pad = lambda s: (s + (block_size - len(s) % block_size) * chr(block_size - len(s) % block_size))
-#     data = pad(data)
-#     print(data)
-#     return  data
-
-# bs = AES.block_size
-# pad = lambda s: s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
-# un_pad = lambda s: s[:-ord(s[len(s) - 1:])]
-#
-# def pading(text):
-#     bs = 16
-#     bytes_1 = text.encode("utf-8")
-#     bytes_1 = bytes(bytes_1)
-#     print('bytes_1%s'%bytes_1)
-#     if len(bytes_1) <= bs:
-#         times =bs-len(bytes_1)
-#         for i in range(times):
-#             print(bytes(str(times),"utf-8"))
-#             bytes_1 += bytes(str(times),"utf-8")
-#     print(bytes_1.decode("utf-8"))
-#     return bytes_1.decode("utf-8")
+# def get_sign(headers):
+#     strhe = 'zns&'
+#     for i in headers.items():
+#         if i[0] == 'token':
+#             strhe = strhe + str(i[1])+'&'
+#         else:
+#             strhe = strhe+ str(i[0]) + '=' +str(i[1]) +'&'
+#     strhe = strhe.rstrip('&')
+#     print(strhe)
+#     return strhe
 
 
 def get_aes(contect,key):
     iv = "16-Bytes--String"
     # 加密模式，可以是CBC、CFB、ECB、OFB等模式
     mode = AES.MODE_CBC
-    by_key = key.encode("utf-8")
-
+    by_key = pad(key.encode(),block_size=16)
+    # by_key = key.encode("utf-8")
     cipher = AES.new(by_key,mode,iv.encode("utf-8"))
     # 完成加密
-    # contect.encode("utf-8")
-    # pand_contect = pad(contect)
-    # pand_contect = pad(contect).encode("utf-8")
-    # pand_contect = bytes(pad(contect),'utf-8')
     pand_contect = pad(contect.encode(),16,style='pkcs7')
-    print(pand_contect)
-    # pand_contect = contect.encode("utf-8")
-    # print(pand_contect)
-    # print(len(pand_contect))
     AES_en_str = cipher.encrypt(pand_contect)
     # 用base64编码一下
     AES_en_str = base64.b64encode(AES_en_str)
@@ -122,17 +87,16 @@ def get_aes(contect,key):
     AES_en_str = AES_en_str.decode("utf-8")
     return AES_en_str
 
-# def aes_decrypt(key,text):
-#     """
-#     解密 ：偏移量为key[0:16]；先base64解，再AES解密，后取消补位
-#     :param encrypted_text : 已经加密的密文
-#     :return:
-#     """
-#     iv = "16-Bytes--String"
-#     encrypted_text = b64decode(text)
-#     cipher = AES.new(key=key.encode(), mode=AES.MODE_CBC, IV=iv.encode())
-#     decrypted_text = cipher.decrypt(encrypted_text)
-#     return un_pad(decrypted_text).decode('utf-8')
+def aes_decrypt(encrypt_data, key):
+    # 解密过程和加密过程相反
+    key = pad(key.encode(), block_size=16,style='pkcs7')
+    aes = AES.new(key, AES.MODE_CBC,IV='16-Bytes--String'.encode())
+    b64_data = base64.decodebytes(encrypt_data.encode(encoding='utf-8'))
+    # 解密
+    decrypt = aes.decrypt(b64_data)
+    # 去掉补齐位，并转换成字符串
+    decrypt_str = unpad(decrypt, block_size=16, style='pkcs7').decode(encoding='utf-8')
+    return decrypt_str
 
 
 
@@ -145,10 +109,15 @@ if __name__ == "__main__":
     # print(headers)
     # print(env)
     # print( str(time.time()).split('.')[0]+'000')
-    print(get_sign(headers = headers))
-    sd = get_aes("mtbsw4@126.com", "0123456789012345")
+    # print(get_sign(headers = headers))
+    sydsa='{"code":"200","msg":"success","data":{"versionCode":"","releaseContent":"","releaseName":"","rnReleaseZipUrl":"","rnReleaseZipMd5":"","minVersion":null,"maxVersion":null,"appType":"","gmtRelease":null,"configJson":""}}'
+    sd = get_aes("mtbsw", "0123456789012345")
     print(sd)
-    # print(aes_decrypt('0123456789012345',sd))
-    print('m8ExrP9lMGCWygajBz2o8Q==')
+    print(aes_decrypt(sd,'0123456789012345'))
+    sd1 = get_aes(sydsa, "012345678901234511232")
+    print(sd1)
+    print(aes_decrypt(sd1, '012345678901234511232'))
+    print(sydsa ==aes_decrypt(sd1, '012345678901234511232') )
+    # print('m8ExrP9lMGCWygajBz2o8Q==')
 
     # print('d'+aes_decrypt('0123456789012345', 'm8ExrP9lMGCWygajBz2o8Q==')+'')
